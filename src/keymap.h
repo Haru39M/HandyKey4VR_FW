577ad9bd1978dd878c2d038d8b7ae4f38697395e
Haru39Mm <harutowakayama3@gmail.com>
2025-11-18 03:46:52 +0900

    FWを右手と左手に分けてみる

--- src/keymap.h
+++ src/keymap.h
@@ -7,8 +7,17 @@
 // -- キーマップの定義 --
 // 右手3段 (uiop, jkl;, m,./)
 // BleKeyboard.press() / release() はASCII文字をそのままキーコードとして扱えます。
+#ifdef RIGHT_HAND
 char keymap[ROWS][COLS] = {
     {'u', 'i', 'o', 'p'}, // ROW 0
     {'j', 'k', 'l', ';'}, // ROW 1
     {'m', ',', '.', '/'}  // ROW 2
 };
+#endif
+#ifdef LEFT_HAND
+char keymap[ROWS][COLS] = {
+    {'q', 'w', 'e', 'r'}, // ROW 0
+    {'a', 's', 'd', 'f'}, // ROW 1
+    {'z', 'x', 'c', 'v'}  // ROW 2
+};
+#endif
